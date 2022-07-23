85f89c438447c491381ff15022ba1b7ae21f0967
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-07-23 11:41:19 +0700

    fix port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.use('/', routes)
 
 app.listen(8000, () => {
-    console.log('Server is running on port ' + 3000)
-    console.log('Visit http://localhost:' + 3000 + '/')
+    console.log('Server is running on port ' + 8000)
+    console.log('Visit http://localhost:' + 8000 + '/')
 })
